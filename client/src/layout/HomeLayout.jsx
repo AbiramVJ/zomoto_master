874fdb0f652f0da735941e846259e4bee026d49f
V.Abiram <70306664+AbiramVJ@users.noreply.github.com>
2021-11-30 12:55:50 +0530

    FoodTab

--- client/src/layout/HomeLayout.jsx
+++ client/src/layout/HomeLayout.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
+import FoodTab from "../components/FoodTab"
 
 
 function HomeLayout({props, children}){
     return(
         <div>
             <Navbar/>
+            <FoodTab/>
             <div className="container mx-auto px-4 lg:px-20">{children}</div>
             
         </div>
