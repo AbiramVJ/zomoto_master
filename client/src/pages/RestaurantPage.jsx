0ef5fbde182b32f47d49b36491c2b5b6c560c826
V.Abiram <70306664+AbiramVJ@users.noreply.github.com>
2021-12-04 21:03:30 +0530

    nutrition page

---
+++ client/src/pages/RestaurantPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+//import ImageGrid from "../components/restaurant/ImageGrid";
+
+function RestaurantPage() {
+  return <div>
+    
+  </div>
+}
+
+export default RestaurantPage;
